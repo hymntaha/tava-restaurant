f074bf9817ea1799a0ca05af0e3c92b4251ae3c2
taha <tahauygun@gmail.com>
2018-06-26 22:41:42 -0500

    creating menu page

--- themes/twentyseventeen/page-menu.php
+++ themes/twentyseventeen/page-menu.php
@@ -5,16 +5,40 @@ require('header.php');
 ?>
 <div class="container-fluid">
     <div class="row">
-        <div class="col-md-6">
-            <img class="rounded mx-auto d-block" src="/wp-content/themes/twentyseventeen/assets/images/kebab-imam.png">
+        <div class="col-md-4">
+            <img class="rounded mx-auto d-block" src="/wp-content/themes/twentyseventeen/assets/images/kofte.jpeg">
         </div>
-        <div class="col-md-6">
-            <img class="rounded mx-auto d-block" src="/wp-content/themes/twentyseventeen/assets/images/kebab-imam.png">
+        <div class="col-md-4">
+            <img class="rounded mx-auto d-block" src="/wp-content/themes/twentyseventeen/assets/images/mucver.jpeg">
+        </div>
+        <div class="col-md-4">
+            <img class="rounded mx-auto d-block" src="/wp-content/themes/twentyseventeen/assets/images/somon.jpeg">
         </div>
     </div>
 </div>
-<div class="container-fluid">
-    <div class="row">
+<?php
+$args = array(
+    'post_type' => 'menu',
+    'category_name' => 'soups'
+);
+$loop = new WP_Query( $args ); ?>
+
+
+<div class="container">
+    <div class="row align-items-center">
+        <div class="col-6 mx-auto">
+            <div class="text-center">
+                <h1 class="display-4">Soups</h1>
+                <?php if ($loop->have_posts()) :
+                    while ($loop->have_posts()) : $loop->the_post(); ?>
+                <p class="lead"><?php echo  the_title(); ?></p>
+                    <?php
+                    endwhile;
+                endif;
+                wp_reset_postdata(); ?>
+            </div>
+        </div>
     </div>
 </div>
+
 <?php get_footer();
